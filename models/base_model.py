c215810e163644a65974405cac485b8ca4f020c3
Erickkaranja <karanja99erick@gmail.com>
2023-03-08 21:59:30 +0300

    task 3 and 4

--- models/base_model.py
+++ models/base_model.py
@@ -8,8 +8,7 @@ from datetime import datetime
 class BaseModel:
     '''initializing class basemodel.'''
 
-    def __init__(self):
-
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -26,9 +25,6 @@ class BaseModel:
 
     def to_dict(self):
         '''returns a dictionary containing all keys/values.'''
-        return {
-                "my_number": number,
-                "name": name,
-                "updated_at": self.updated_at.isoformat(),
-                "created_at": self.created_at.isoformat()
-                }
+        self.created_at = self.created_at.isoformat()
+        self.updated_at = self.updated_at.isoformat()
+        return self.__dict__
