c5a0b150b89d5d6db6dfed2ef78471d0e8de294f
Erickkaranja <karanja99erick@gmail.com>
2023-03-08 16:37:12 +0300

    task 3

--- console.py
+++ console.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python3
+'''initializing class HBNBCommand.'''
 
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
     '''implementing class HBNBCommand.'''
-    prompt  = '(hbnb)'
+    prompt = '(hbnb) '
+
+    def do_EOF(self, line):
+        '''quits the program through an interrupt ctrl+D'''
+        print ("")
+        return True
+
+    def do_quit(self, line):
+        '''Quit command to exit the program'''
+        return True
+
+    def emptyline(self):
+        print
+
+    def postloop(self):
+        print
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
---
+++ models/base_model.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+'''Defining class BaseModel.'''
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    '''initializing class basemodel.'''
+
+    def __init__(self):
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        '''prints the class name, id and dict representation of object.'''
+        return "[{}] ({}) {}".format(__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        '''updates the public instance attribute updated_at with the current
+            datetime.'''
+        updated_at = datetime.now()
+
+    def to_dict(self):
+        '''returns a dictionary containing all keys/values.'''
+        return {
+                "my_number": number,
+                "name": name,
+                "updated_at": self.updated_at.isoformat(),
+                "created_at": self.created_at.isoformat()
+                }
